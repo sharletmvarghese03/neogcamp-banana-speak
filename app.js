c5c47ed037764be27f88e6fe738b37b6f4aa9665
Sharlet Varghese <sharletmvarghese03@gmail.com>
2023-05-17 15:44:22 +0530

    feat: updated serverURL to minion translation

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var txtInput = document.querySelector('#txt-input');
 
 var outputDiv = document.querySelector('#output');
 
-var serverURL = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
+// var serverURL = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
+
+var serverURL = 'https://api.funtranslations.com/translate/minion.json';
 
 function getTranslationURL(text) {
 	return serverURL + '?' + 'text=' + text;
