daeacd75ee28823a05e0614a168214de9b2e620f
Sharlet Varghese <sharletmvarghese03@gmail.com>
2023-05-17 15:53:36 +0530

    refactor: codes updated

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var outputDiv = document.querySelector('#output');
 
 var serverURL = 'https://api.funtranslations.com/translate/minion.json';
 
-function getTranslationURL(text) {
-	return serverURL + '?' + 'text=' + text;
+function getTranslationURL(input) {
+	return serverURL + '?' + 'text=' + input;
 }
 
 function errorHandler(error) {
 	console.log('error occured', error);
-	alert('something wrong with server! try after sometime');
+	alert('something wrong with server! try again after some time');
 }
 
 function clickHandler() {
@@ -21,7 +21,10 @@ function clickHandler() {
 
 	fetch(getTranslationURL(inputText))
 		.then((response) => response.json())
-		.then((json) => console.log(json.contents.translated))
+		.then((json) => {
+			var translatedText = json.contents.translated;
+			outputDiv.innerText = translatedText;
+		})
 		.catch(errorHandler);
 }
 
